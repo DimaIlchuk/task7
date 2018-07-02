f4e2fe20700fb831fb52166fed62a4df3f101e05
unknown <ilchukdima04@gmail.com>
2018-07-02 23:00:59 +0300

    Activity with button. On button click, add notification to status bar, on next click - remove notification. Click on notification should open our activity.

---
+++ app/src/main/java/com/example/ilchu/myapplication7/Result.java
@@ -0,0 +1,14 @@
+package com.example.ilchu.myapplication7;
+
+import android.os.Bundle;
+import android.app.Activity;
+
+public class Result extends Activity{
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+    }
+
+}
